8ebad479057263da2fb4c23a25adea30fa03671c
Mohammed Tahir Jameel <mohammedtahirjameel@gmail.com>
2026-01-14 15:16:44 +0530

    Amazon E-Commerce working

--- src/main/java/com/tahir/automation/pages/MainPage.java
+++ src/main/java/com/tahir/automation/pages/MainPage.java
@@ -3,9 +3,12 @@ package com.tahir.automation.pages;
 import java.time.Duration;
 import java.util.List;
 
+import javax.swing.Action;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -17,6 +20,24 @@ public class MainPage {
     public WebDriver driver;
 
     // Locators
+    @FindBy(id = "nav-link-accountList")
+    private WebElement NavToSignIn;
+
+    @FindBy(className  = "nav-action-inner")
+    private WebElement FloatingSignIn;
+    
+    @FindBy(id = "ap_email_login")
+    private WebElement EmailorPhone;
+
+    @FindBy(className  = "a-button-input")
+    private WebElement ContinueButton;
+
+    @FindBy(id = "ap_password")
+    private WebElement PwdField;
+
+    @FindBy(id = "signInSubmit")
+    private WebElement FinalSignInButton;
+
     @FindBy(id = "twotabsearchtextbox")
     private WebElement SearchBox;
     
@@ -26,6 +47,18 @@ public class MainPage {
      @FindBy(className =  "a-dropdown-container")
     private WebElement sortbyCN;
 
+    @FindBy(xpath =  "//span[@id='submit.add-to-cart-announce']")
+    private WebElement AddToCartButton;
+
+    @FindBy(id = "wishListMainButton")
+    private WebElement AddToWishlistButton;
+
+    @FindBy(id  = "huc-view-your-list-button")
+    private WebElement ViewYourListButton;
+
+    @FindBy(id = "itemName_I11PTUNO0CNEMT")
+    private WebElement ClickOnTheProduct;
+
 
     // Constructor
     public MainPage(WebDriver driver) {
@@ -34,6 +67,21 @@ public class MainPage {
     }
 
     // Actions
+
+    public void OpenAmazonAndLogin(String username, String password) {
+        // Implementation for login can be added here if needed
+        Actions actions = new Actions(driver);
+        actions.moveToElement(NavToSignIn).perform();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement signInButton = wait.until(
+        ExpectedConditions.elementToBeClickable(FloatingSignIn));
+        signInButton.click();
+
+        EmailorPhone.sendKeys(username);
+        ContinueButton.click();
+        PwdField.sendKeys(password);
+        FinalSignInButton.click();
+    }
    
 public void SearchProductByName(String productName) {
         SearchBox.sendKeys(productName);
@@ -50,6 +98,43 @@ public void SortProductBy(String SortBy) {
     }
 
     public void ClickOnTheProduct(String ProductToBeClicked) {
-        // Implementation for clicking on a specific product
+        //click on the product with name passed from config file and new tab will be opened and switch to that tab
+
+        By productLocator = By.xpath("//span[contains(text(), '" + ProductToBeClicked + "')]");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement productElement = wait.until(ExpectedConditions.elementToBeClickable(productLocator));
+        productElement.click();
+        // Switch to the new tab
+        List<String> tabs = List.copyOf(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
     }
+
+    public void AddProductToWishlist() {
+        // Implementation for adding product to wishlist can be added here if needed
+        AddToWishlistButton.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement viewListButton = wait.until(
+        ExpectedConditions.elementToBeClickable(ViewYourListButton));
+        viewListButton.click();
+    }
+
+    public void AddProductToCart() throws InterruptedException {
+        AddToCartButton.click();
+        Thread.sleep(10000);
+
+    }
+
+    public void verifyProductAddedToWishlist(String wishlistProduct) {
+        // Implementation for verifying product added to wishlist can be added here if needed
+        String wishlistProductName01 = ClickOnTheProduct.getText();
+        System.out.println("Product added to wishlist: " + wishlistProductName01);    
+        wishlistProductName01.equals(wishlistProduct);
+    }
+
+    // public void TakeScreenshot(String fileName) {
+    //     // Implementation for taking screenshot can be added here if needed
+    //     // ScreenshotUtil.takeScreenshot(driver, fileName);
+    //     TakeScreenshot(fileName);   
+
+    // }
 }
--- src/test/java/com/tahir/automation/tests/LoginTest.java
+++ src/test/java/com/tahir/automation/tests/LoginTest.java
@@ -37,7 +37,17 @@ public class LoginTest extends BaseTest {
     //     Assert.assertEquals(driver.getTitle(), expectedTitle, "Page1 title mismatch!");
     // }
 
-    @Test(priority = 1, description = "Search for product & Verify Page Title")
+    @Test(priority = 1, description = "Open Amazon Home Page & Login")
+    public void OpenAmazonAndLogin() {
+        String url = ConfigReader.get("url");
+        driver.get(url);
+        String username = ConfigReader.get("username");
+        String password = ConfigReader.get("password");
+        MainPage main = new MainPage(driver);
+        main.OpenAmazonAndLogin(username, password);
+    }
+
+    @Test(priority = 2, description = "Search for product & Verify Page Title")
     public void GotoProduct() throws InterruptedException {
         String productName = ConfigReader.get("productsName");
         MainPage main = new MainPage(driver);
@@ -47,13 +57,59 @@ public class LoginTest extends BaseTest {
         Assert.assertEquals(driver.getTitle(), expectedTitle2, "Page2 title mismatch!");
     }
 
-    @Test(priority = 2, description = "Sort Product By")
+    @Test(priority = 3, description = "Sort Product By")
     public void SortProductBy() {
         String sortBy = ConfigReader.get("SortBy");
         MainPage main = new MainPage(driver);
         main.SortProductBy(sortBy);
     }
-        
 
+    @Test(priority = 4, description = "Click On The Product")
+        public void ClickOnTheProduct() {
+        String productToBeClicked = ConfigReader.get("ProductToBeClicked");
+        MainPage main = new MainPage(driver);
+        main.ClickOnTheProduct(productToBeClicked);
+    }
+
+    @Test(priority = 5, description = "Verify Product Page Title")
+    public void verifyProductPageTitle() {
+        String expectedProductPageTitle = ConfigReader.get("ProductPageTitle");
+        System.out.println("Product Page Title: " + driver.getTitle());
+        Assert.assertEquals(driver.getTitle(), expectedProductPageTitle, "Product Page title mismatch!");
+    }
+
+    // @Test(priority = 6, description = "Add Product to Cart")
+    // public void AddProductToCart() throws InterruptedException {
+    //     MainPage main = new MainPage(driver);
+    //     main.AddProductToCart();
+    // }
+
+    @Test(priority = 6, description = "Add to Wishlist")
+    public void AddProductToWishlist() throws InterruptedException {
+        MainPage main = new MainPage(driver);
+        main.AddProductToWishlist();
+    }
+
+    @Test(priority = 7, description = "Verify Product Added to Wishlist")
+    public void verifyProductAddedToWishlist() {
+        String wishlistProduct = ConfigReader.get("ProductToBeClicked");
+        MainPage main = new MainPage(driver);
+        main.verifyProductAddedToWishlist(wishlistProduct);
+        
+    }
+
+
+    @Test(priority = 8, description = "wait after adding to Wishlist")
+    public void waitAfterAddingToWishlist() throws InterruptedException {
+        Thread.sleep(10000);
+    }
+
+
+
+    // @Test(priority = 8, description = "Take Screenshot After Adding to Wishlist")
+    // public void TakeScreenshotAfterAddingToWishlist() throws InterruptedException {
+    //     MainPage main = new MainPage(driver);
+    //     main.TakeScreenshot("AddToWishlist");
+    // }
 
 }
